b79d719bbea6d0cf2d47bdd5a90303b5d9e2e683
Michael C <michristmann@hotmail.com>
2022-02-21 18:15:46 -0300

    Add: fake api (miragejs)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { createServer } from "miragejs"
+
 import { App } from "./App"
 import { GlobalStyle } from "./styles/global"
+
+createServer({
+  routes() {
+    this.namespace = "api"
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          type: "deposit",
+          category: "food",
+          createdAt: new Date(),
+        },
+      ]
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
