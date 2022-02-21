8af196e8a3efc5a189a9fc129602b71a25804914
Michael C <michristmann@hotmail.com>
2022-02-21 16:10:55 -0300

    Add: global style & assets

---
+++ src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f3f5;
+    --shape: #ffffff;
+
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    
+    --text-tile: #363f5f;
+    --text-body: #969cb3;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600; 
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
