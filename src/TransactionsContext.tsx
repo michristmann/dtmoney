f6454372d866d2ceefe98285a04d2cfde5444e84
Michael C <michristmann@hotmail.com>
2022-06-01 17:18:16 -0300

    Add: transacition context

---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import { api } from "./services/api"
+
+interface Transaction {
+  id: number
+  title: string
+  value: string
+  category: string
+  type: string
+  createdAt: string
+}
+
+type TransactionInputProps = Omit<Transaction, "id" | "createdAt">
+
+interface TransactionsProviderProps {
+  children: ReactNode
+}
+
+interface TransactionsContextDataProps {
+  transactions: Transaction[]
+  createTransaction: (transaction: TransactionInputProps) => Promise<void>
+}
+
+export const TransactionsContext = createContext<TransactionsContextDataProps>(
+  {} as TransactionsContextDataProps
+)
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions))
+  }, [])
+
+  async function createTransaction(transactionInput: TransactionInputProps) {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data
+
+    setTransactions([...transactions, transaction])
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
